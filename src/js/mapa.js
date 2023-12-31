3f1548e5b37d9f3d8feb80c417002dc6f06cc38f
Cano Freire <jx.cano@ibermatica.com>
2023-12-31 12:50:15 +0100

    Se ha añadido la función para guardas las ciudades que queramos

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,23 +1,34 @@
-// Creamos el mapa
-var map = L.map('mapa').setView([43.1611, -2.5873], 9);
+/* Creamos el mapa y le indicamos la vista inicial */
+var map = L.map('mapa').setView([43.1611, -2.5873], 9)
 
 // Le añadimos una marca de agua
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+}).addTo(map)
 
-// Creamos los marcadores
-
+/* Creamos los marcadores: */
 // San Sebastian
-L.marker([43.34, -1.98]).addTo(map);
+L.marker([43.34, -1.98])
+    .addTo(map)
+    .on("click", function () { añadir("San Sebastián") })
 
 // Bilbao
-L.marker([43.2655, -2.9354]).addTo(map);
+L.marker([43.2655, -2.9354])
+    .addTo(map)
+    .on("click", function () { añadir("Bilbao") })
 
 // Vitoria Gazteiz
-L.marker([42.8463, -2.6738]).addTo(map);
+L.marker([42.8463, -2.6738])
+    .addTo(map)
+    .on("click", function () { añadir("Vitoria Gasteiz") })
 
-$(document).ready($("#iconoMinimizar").click(function () { minimizar() }))
+if (localStorage.getItem("ciudades") != undefined) {
+    $(document).ready($("#iconoMinimizar").click(function () { maximizar() }))
+    $("#mapa").css({ "width": "0px", "height": "0px" })
+} else {
+    $(document).ready($("#iconoMinimizar").click(function () { minimizar() }))
+    $("#mapa").css({ "width": "90%", "height": "500px" })
+}
 
 function minimizar() {
     $("#mapa").css({ "width": "0px", "height": "0px" })
@@ -27,4 +38,20 @@ function minimizar() {
 function maximizar() {
     $("#mapa").css({ "width": "90%", "height": "500px" })
     $("#iconoMinimizar").click(function () { minimizar() })
+}
+
+function añadir(ciudad) {
+    if (confirm(`¿Quieres ver el tiempo en ${ciudad}?`)) {
+        switch (true) {
+            case localStorage.getItem("ciudades") == undefined:
+                localStorage.setItem("ciudades", ciudad)
+                break
+            case !localStorage.getItem("ciudades").includes(ciudad):
+                localStorage.setItem("ciudades", `${localStorage.getItem("ciudades")};${ciudad}`)
+                break
+            case localStorage.getItem("ciudades").includes(ciudad):
+                alert(`${ciudad} ya está registrada para mostrar`)
+                break
+        }
+    }
 }
